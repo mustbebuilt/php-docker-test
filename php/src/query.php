795bad19fc4aa5e0a8e28318f71b84f3ecc7e4ca
mustbebuilt <martin@mustbebuilt.co.uk>
2022-02-07 10:17:08 +0000

    Added include connection

--- php/src/query.php
+++ php/src/query.php
@@ -1,24 +1,24 @@
 <?php
-$host = 'db';
-$user = 'MYSQL_USER';
-//database user password
-$pass = 'MYSQL_PASSWORD';
-// database name
-$mydatabase = 'MYSQL_DATABASE';
-// check the mysql connection status
-$conn = new mysqli($host, $user, $pass, $mydatabase);
-
-$sql = 'SELECT * FROM movies';
-
-if ($result = $conn->query($sql)) {
-    while ($data = $result->fetch_object()) {
-        $movies[] = $data;
-    }
-}
-
-foreach ($movies as $movie) {
-    echo "<br>";
-    echo $movie->filmName . " " . $movie->filmCertificate;
-    echo "<br>";
+include("conn.inc.php");
+$query = 'SELECT * FROM movies';
+$result = mysqli_query($connection, $query);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Film List Query</title>
+</head>
+<body>
+    <h1>Film List</h1>
+    <ul>
+<?php while($row = mysqli_fetch_array($result)){
+echo "<li>{$row['filmName']}</li>";
 }
 ?>
+</ul>
+</body>
+</html>
+
